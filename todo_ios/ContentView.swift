43c6d2fc36a3fd815cf85dd93a8ebfe6a5009db0
Dian Gao <diangao@umich.edu>
2024-10-27 23:51:18 -0400

    Initial Commit

---
+++ todo_ios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  todo_ios
+//
+//  Created by Diyan Gao on 10/27/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ todo_ios/todo_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  todo_iosApp.swift
+//  todo_ios
+//
+//  Created by Diyan Gao on 10/27/24.
+//
+
+import SwiftUI
+
+@main
+struct todo_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
